bd7bb1c2e58b4b1cac5e17b6386132028398537c
Kiet Cao <kietcao@tuta.io>
2024-08-04 23:15:54 +0900

    add counter with redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,25 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
+
+  const onIncrement = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const onDecrement = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+
+const store = createStore(
+  (
+    state = {
+      counter: 0,
+    },
+    action,
+  ) => {
+    if (action.type === "increment") {
+      return {
+        counter: state.counter + 1,
+      };
+    }
+    if (action.type === "decrement") {
+      return {
+        counter: state.counter - 1,
+      };
+    }
+    return state;
+  },
+);
+
+export default store;
